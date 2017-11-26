250e396c5ab489058a50f6cb6e27953f9d80a247
Unknown <ibrik@mail.ru>
2017-11-26 14:29:24 +0200

    Revert "Added Leave me alone, world output"
    This reverts commit 482423d62fa938381a79536d338db37480af14bb.

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,5 @@
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world");
-    System.out.println("Leave me alone, world");
+    System.out.println("Hello, world");  
   }
 }
