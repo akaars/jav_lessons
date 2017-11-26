5bcf6871214441c137b24c051156b765b16eedfc
Ilya Brik <ilyabr@gmail.com>
2017-11-26 21:02:24 +0200

    Created my first package

--- sandbox/src/main/java/il/stqa/jav/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/il/stqa/jav/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
+package il.stqa.jav.sandbox;
+
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world");  
+    System.out.println("Hello, world");
   }
 }
