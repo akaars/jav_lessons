195437936864e1d4760f21f5b3d90e40bffea757
Unknown <ibrik@mail.ru>
2017-11-27 16:36:41 +0200

    Several  functions were added

--- sandbox/src/main/java/il/stqa/jav/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/il/stqa/jav/sandbox/MyFirstProgram.java
@@ -2,6 +2,25 @@ package il.stqa.jav.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    hello("world");
+    hello("Ilya");
+    double len = 5;
+    System.out.println("Square's area with side " + (int)len + " is: " + (int)area(len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Rectangle's area with sides " + (int)a + " and " + (int)b + " is: " + (int)area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area (double a, double b){
+    return a * b;
   }
 }
