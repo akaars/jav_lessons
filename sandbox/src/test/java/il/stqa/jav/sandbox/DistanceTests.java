2b73d0dbb893e7da199cc71be3e2a4c136cb04f8
Ilya Brik <ilyabr@gmail.com>
2017-11-27 22:29:23 +0200

    My first test using TestNG
    Testing distamce measurement withing the Point class using TestNG FW

---
+++ sandbox/src/test/java/il/stqa/jav/sandbox/DistanceTests.java
@@ -0,0 +1,29 @@
+package il.stqa.jav.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DistanceTests {
+    @Test
+    public void testFunction1() {
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(6,1);
+        Assert.assertEquals(p1.x, 1.0);
+        Assert.assertEquals(p2.x, 6.0);
+        Assert.assertEquals(p1.distance(p2), 5.0);
+    }
+
+    @Test
+    public void testFunction2() {
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(0,0);
+        Assert.assertEquals(p1.distance(p2), 0.0);
+    }
+
+    @Test
+    public void testFunction3() {
+        Point p1 = new Point(3,4);
+        Point p2 = new Point(0,0);
+        Assert.assertEquals(p2.distance(p1), 5.0);
+    }
+}
