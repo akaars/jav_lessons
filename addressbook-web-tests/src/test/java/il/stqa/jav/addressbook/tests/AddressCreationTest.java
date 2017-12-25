670608fe8419999a6bd94f9f9977e82277518263
Ilya Brik <ilyabr@gmail.com>
2017-12-25 22:20:13 +0200

    replacing multiple functions by a single one for testNewAddressCreation test

--- addressbook-web-tests/src/test/java/il/stqa/jav/addressbook/tests/AddressCreationTest.java
+++ addressbook-web-tests/src/test/java/il/stqa/jav/addressbook/tests/AddressCreationTest.java
@@ -8,15 +8,7 @@ public class AddressCreationTest extends TestBase{
 
   @Test
   public void testNewAddressCreation() {
-    app.getAddrHelper().gotoNewAddrLink();
-    app.getAddrHelper().fillAddrForm(new AddressForm("John", "D", "Dow", null,
+    app.getAddrHelper().createNewAddrEntry(new AddressForm("John", "D", "Dow", null,
             "Mr", "test1",null, "+44-2-12-85-06", "test@mail.ru"));
-    app.getNavigationHelper().submit();
-    app.getNavigationHelper().backToHome();
   }
-
-
-
-
-
 }
