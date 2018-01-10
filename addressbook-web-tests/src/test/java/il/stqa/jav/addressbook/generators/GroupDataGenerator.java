ef43ec208af17daffcfa34f9911cdd2fd4b32e32
Ilya Brik <ilyabr@gmail.com>
2018-01-10 21:32:27 +0200

    Read and write Json and XML formats
    Generate XML and JSON test data sets.
    Load the files and use the loaded content as test input for GroupsCreation test

--- addressbook-web-tests/src/test/java/il/stqa/jav/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/il/stqa/jav/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,9 @@ package il.stqa.jav.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
 import il.stqa.jav.addressbook.model.GroupForm;
 
 import java.io.File;
@@ -10,7 +13,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -23,6 +25,9 @@ public class GroupDataGenerator {
   @Parameter(names = "-f", description = "Target file name")
   public String file;
 
+  @Parameter(names = "-d", description = "Target file format")
+  public String format;
+
   public static void main(String[] args) throws IOException {
     GroupDataGenerator generator = new GroupDataGenerator();
     try {
@@ -31,18 +36,42 @@ public class GroupDataGenerator {
       ex.usage();
       return;
     }
-
-//    int count = Integer.parseInt(args[0]);
-//    File file = new File(args[1]);
     generator.run();
   }
 
   private void run() throws IOException {
     List<GroupForm> groups = generateGroup(count);
-    save(groups, new File(file));
+    if (format.equals("csv")){
+      saveAsCsv(groups, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(groups, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJson(groups, new File(file));
+    } else {
+      System.out.println("Unsupported file format" + format);
+      return;
+    }
   }
 
-  private void save(List<GroupForm> groups, File file) throws IOException {
+  private void saveAsJson(List<GroupForm> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting()
+            .create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
+  }
+
+  private void saveAsXml(List<GroupForm> groups, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupForm.class);
+    String xml = xstream.toXML(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  private void saveAsCsv(List<GroupForm> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for (GroupForm group: groups) {
