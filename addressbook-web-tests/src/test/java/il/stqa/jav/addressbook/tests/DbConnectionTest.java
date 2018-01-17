615e2e7bf89bc997dcfcc69e8130e451fe111674
Ilya Brik <ilyabr@gmail.com>
2018-01-17 22:51:03 +0200

    DbConnectionTest
    Basic mysql connectivity test

--- addressbook-web-tests/src/test/java/il/stqa/jav/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/il/stqa/jav/addressbook/tests/DbConnectionTest.java
@@ -1,10 +1,10 @@
 package il.stqa.jav.addressbook.tests;
 
+import il.stqa.jav.addressbook.model.GroupForm;
+import il.stqa.jav.addressbook.model.Groups;
 import org.testng.annotations.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Created by ilya on 1/17/18
@@ -12,12 +12,28 @@ import java.sql.SQLException;
 public class DbConnectionTest {
   @Test
   public void testDbConnection() {
+    Connection conn = null;
+    Groups groups = new Groups();
     try{
-      Connection con= DriverManager.getConnection("jdbc:mysql://192.168.56.101:3306/addressbook",
-              "root","");
-    } catch (SQLException e) {
-      e.printStackTrace();
+      conn= DriverManager.getConnection("jdbc:mysql://192.168.56.101:3306/addressbook", "root",
+              "test");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
+      while(rs.next()) {
+        groups.add(new GroupForm().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                .withFooter(rs.getString("group_footer")).withHeader(rs.getString("group_header")));
+      }
+      System.out.println("resuts is: " + groups);
+      rs.close();
+      st.close();
+      conn.close();
+
+    } catch (SQLException ex) {
+      System.out.println("SQLException " + ex.getMessage());
+      System.out.println("SQLState " + ex.getSQLState());
+      System.out.println("Vendor error " + ex.getErrorCode());
     }
+
   }
 
 }
