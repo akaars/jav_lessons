d98e8809b90ff00d83638878c742b7ac165e0ca0
Ilya Brik <ibrik@mail.ru>
2018-01-18 10:24:29 +0200

    Using ORM - first steps

--- addressbook-web-tests/src/test/java/il/stqa/jav/addressbook/model/GroupForm.java
+++ addressbook-web-tests/src/test/java/il/stqa/jav/addressbook/model/GroupForm.java
@@ -3,18 +3,32 @@ package il.stqa.jav.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @XStreamAlias("group")
+@Entity
+@Table(name="group_list")
 public class GroupForm {
   @XStreamOmitField
+  @Id
+  @Column(name="group_id")
   private int groupId = Integer.MAX_VALUE;
   @Expose
+  @Column(name="group_name")
   private String groupName;
   @Expose
+  @Column(name="group_header")
+  @Type(type="text")
   private String header;
   @Expose
+  @Column(name="group_footer")
+  @Type(type="text")
   private String footer;
 
   public GroupForm withId(int groupId) {
