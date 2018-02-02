4d7a30fb84b6901a9655749d3dd27cec67fa2882
Ilya Brik <ilyabr@gmail.com>
2018-02-02 17:01:25 +0200

    Adding remote capabilities, allow remote run

--- addressbook-web-tests/src/test/java/il/stqa/jav/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/il/stqa/jav/addressbook/appmanager/ApplicationManager.java
@@ -1,17 +1,21 @@
 package il.stqa.jav.addressbook.appmanager;
 
 import org.apache.commons.lang3.SystemUtils;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -40,33 +44,39 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     dbHelper = new DbHelper();
-
-    if (browser.equals(BrowserType.FIREFOX)) {
-      String ffLocation = new String();
-      if (SystemUtils.IS_OS_LINUX) {
-        ffLocation = properties.getProperty("ff.linuxLocation");
-      } else if (SystemUtils.IS_OS_MAC) {
-        ffLocation = properties.getProperty("ff.macLocation");
+    if("".equals(properties.getProperty("selenium.server"))){
+      if (browser.equals(BrowserType.FIREFOX)) {
+        String ffLocation = new String();
+        if (SystemUtils.IS_OS_LINUX) {
+          ffLocation = properties.getProperty("ff.linuxLocation");
+        } else if (SystemUtils.IS_OS_MAC) {
+          ffLocation = properties.getProperty("ff.macLocation");
+        }
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments(String.format("window-size=%s", properties.getProperty("web.windowSize")));
+        options.setLegacy(true);
+        options.setBinary(ffLocation);
+        if (isHeadless) {
+          options.setHeadless(true);
+        }
+        wd = new FirefoxDriver(options);
+      } else if (browser.equals(BrowserType.CHROME)) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(String.format("window-size=%s", properties.getProperty("web.windowSize")));
+        if (isHeadless) {
+          options.setHeadless(true);
+        }
+        wd = new ChromeDriver(options);
+      } else {
+        System.out.println("Unsupported browser");
+        System.exit(1);
       }
-      FirefoxOptions options = new FirefoxOptions();
-      options.addArguments(String.format("window-size=%s", properties.getProperty("web.windowSize")));
-      options.setLegacy(true);
-      options.setBinary(ffLocation);
-      if (isHeadless) {
-        options.setHeadless(true);
-      }
-      wd = new FirefoxDriver(options);
-    } else if (browser.equals(BrowserType.CHROME)) {
-      ChromeOptions options = new ChromeOptions();
-      options.addArguments(String.format("window-size=%s", properties.getProperty("web.windowSize")));
-      if (isHeadless) {
-        options.setHeadless(true);
-      }
-      wd = new ChromeDriver(options);
-    } else {
-      System.out.println("Unsupported browser");
-      System.exit(1);
+    }else{
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
+
 
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
